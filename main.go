3dc4b9ab7d1eccea305321439bdb8886d8fadf97
icoffee <1596740959@qq.com>
2025-05-17 17:25:34 +0800

    view&pdf&download

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"path/filepath"
+)
 
 func main() {
-    r := gin.Default()
-    r.GET("/", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "Hello, Gin!",
-        })
-    })
-    r.Run() // 默认监听 :8080
+	r := gin.Default()
+
+	// 静态资源目录
+	r.Static("/static", "./static")
+	r.LoadHTMLGlob("templates/*")
+
+	// 模拟发票页面
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "invoice.html", gin.H{
+			"invoiceNo": "25952000000093739161",
+		})
+	})
+
+	// 模拟下载接口
+	r.GET("/download-pdf/:invoiceNo", func(c *gin.Context) {
+		invoiceNo := c.Param("invoiceNo")
+		filename := invoiceNo + "_发票.pdf"
+		filepath := filepath.Join("static", filename)
+		c.FileAttachment(filepath, filename)
+	})
+
+	r.Run(":8080")
 }
