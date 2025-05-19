9539e27c53277493f1fcb0e6564a0b460be498f5
icoffee <1596740959@qq.com>
2025-05-19 11:18:00 +0800

    save

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	r.GET("/download-pdf/:invoiceNo", func(c *gin.Context) {
 		// 实际只返回固定文件
 		filepath := filepath.Join("static", "dzfp_25952000000093739161_特灵科技（中国）有限公司_20250514154937.pdf")
-		c.FileAttachment(filepath, "downloaded_invoice.pdf")
+		c.FileAttachment(filepath, "dzfp_25952000000093739161_特灵科技（中国）有限公司_20250514154937.pdf")
 	})
 
 	r.Run(":8080")
