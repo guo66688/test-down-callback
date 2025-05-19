0abef20156e9f86eb4d3ff2d13aa9210fc511d34
icoffee <1596740959@qq.com>
2025-05-19 11:16:43 +0800

    fix_pdf_name_rename

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	// 提供一个固定 PDF 文件供下载
 	r.GET("/download-pdf/:invoiceNo", func(c *gin.Context) {
 		// 实际只返回固定文件
-		filepath := filepath.Join("static", "dummy.pdf")
+		filepath := filepath.Join("static", "dzfp_25952000000093739161_特灵科技（中国）有限公司_20250514154937.pdf")
 		c.FileAttachment(filepath, "downloaded_invoice.pdf")
 	})
 
